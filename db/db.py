11ef9e77c9f3acc5f532e242deb3266e7594b07a
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-13 14:05:55 +0530

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,8 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URI
 
-client = AsyncIOMotorClient(MONGO_URI)
-db = client["pxl_ads_db"]
-
-publishers = db["publishers"]
-ads = db["ads"]
+try:
+    client = AsyncIOMotorClient(MONGO_URI)
+    db = client["pxl_ads_db"]
+    publishers = db["publishers"]
+    ads = db["ads"]
+except Exception as e:
+    print(f"Error: {e}")
