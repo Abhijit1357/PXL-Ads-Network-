7f11fc8a9ef824bcf38348b5ad07760a4e6b3049
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-13 10:26:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_HASH = os.getenv("API_HASH", "f400bc1d1baeb9ae93014ce3ee5ea835")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://AOMusicBot:AOMusicBot@cluster0.sibxiqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001234567890"))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002134425165"))
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "6148346742").split()))
 
 # CPM Values (example: ₹10 per 1000 impressions)
