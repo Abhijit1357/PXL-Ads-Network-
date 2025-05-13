b81096566ada03502abb5a9199844341ee55a61e
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-13 13:33:05 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,10 +1,10 @@
-from aiogram import types, Router
+from aiogram import Router
 from aiogram.types import Message
+from aiogram.filters import Command
 from templates.info_texts import WELCOME_TEXT
 
 router = Router()
 
-# Use a filter to check if the message text is "start" or "help"
-@router.message(lambda message: message.text.lower() in ["start", "help"])
+@router.message(Command(commands=["start", "help"]))
 async def start_handler(msg: Message):
     await msg.answer(WELCOME_TEXT.format(name=msg.from_user.full_name))
