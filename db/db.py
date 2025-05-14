c3cf9dd51d132d5360109c3c1c962ab06f1c0fd0
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-14 19:53:18 +0530

    Update db.py

--- db/db.py
+++ db/db.py
@@ -3,8 +3,16 @@ from config import MONGO_URI
 
 try:
     client = AsyncIOMotorClient(MONGO_URI)
-    db = client["pxl_ads_db"]
+    db = client.get_default_database() or client["pxl_ads_db"]
+
+    # Collections
     publishers = db["publishers"]
     ads = db["ads"]
+
+    print("[MongoDB] Connected successfully.")
 except Exception as e:
-    print(f"Error: {e}")
+    print(f"[MongoDB] Connection Error: {e}")
+    client = None
+    db = None
+    publishers = None
+    ads = None
