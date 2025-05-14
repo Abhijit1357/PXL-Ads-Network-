e2a68f55264add98ce4143cf3230ebe208c71757
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-14 15:12:53 +0530

    Update publisher.py

--- handlers/inline/keyboards/publisher.py
+++ handlers/inline/keyboards/publisher.py
@@ -1,10 +1,14 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_publisher_panel_keyboard():
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="📊 Dashboard", callback_data="dashboard")],
-        [InlineKeyboardButton(text="🤖 Bot Monetization", callback_data="bot_monetization")],
-        [InlineKeyboardButton(text="💳 Withdraw", callback_data="withdraw")],
-        [InlineKeyboardButton(text="◀️ Back", callback_data="go_back")]
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton("📊 Dashboard", callback_data="dashboard")],
+        [InlineKeyboardButton("🤖 Bot Monetization", callback_data="bot_monetization")],
+        [InlineKeyboardButton("💳 Withdraw", callback_data="withdraw")],
+        [InlineKeyboardButton("◀️ Back", callback_data="go_back")]
     ])
-    return keyboard
+
+def get_register_keyboard():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton("✅ Accept & Register", callback_data="register_accept")]
+    ])
