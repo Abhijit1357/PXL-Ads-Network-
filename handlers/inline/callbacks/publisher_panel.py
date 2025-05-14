b46e91aa20b5314f0363c709fae4a3df492e1772
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-14 11:00:56 +0530

    Create publisher_panel.py

---
+++ handlers/inline/callbacks/publisher_panel.py
@@ -0,0 +1,12 @@
+from aiogram import Router
+from aiogram.types import CallbackQuery
+from handlers.inline.keyboards.publisher import get_publisher_panel_keyboard
+
+router = Router()
+
+@router.callback_query(lambda x: x.data == "publisher_panel")
+async def publisher_panel_cb(callback: CallbackQuery):
+    keyboard = get_publisher_panel_keyboard()
+    text = """Publisher Panel"""
+    await callback.message.edit_text(text, reply_markup=keyboard)
+    await callback.answer()
