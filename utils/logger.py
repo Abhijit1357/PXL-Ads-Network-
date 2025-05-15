9a43e6ef2d8e0938da4b6ec987094247442ced08
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-15 10:08:36 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,9 @@
+from aiogram import Bot
+from config import LOG_GROUP_ID
+
+async def log_to_group(bot: Bot, message: str):
+    if LOG_GROUP_ID:
+        try:
+            await bot.send_message(LOG_GROUP_ID, message, parse_mode="HTML")
+        except Exception as e:
+            print(f"[Logger Error] {e}")
