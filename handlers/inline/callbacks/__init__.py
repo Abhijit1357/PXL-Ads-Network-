2774630e2f8545a2336bbac4f4241e61d8e6f7cf
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-15 13:08:20 +0530

    Update __init__.py

--- handlers/inline/callbacks/__init__.py
+++ handlers/inline/callbacks/__init__.py
@@ -1,8 +1,14 @@
-from .register import router as register_router
+from aiogram import Router
+from .profile import router as profile_router
+from .panels import router as panels_router
+from .general import router as general_router
+from .publisher_panel import router as publisher_panel_router
+from .register import router as register_router   # <-- Register router import
 
 inline_callbacks_router = Router()
+
 inline_callbacks_router.include_router(profile_router)
 inline_callbacks_router.include_router(panels_router)
 inline_callbacks_router.include_router(general_router)
 inline_callbacks_router.include_router(publisher_panel_router)
-inline_callbacks_router.include_router(register_router)  # <---- add this line
+inline_callbacks_router.include_router(register_router)  # <-- Register router included here
