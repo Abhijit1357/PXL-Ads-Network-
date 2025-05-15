d60cc361c8ca354712457a58dd8bbfbe492e969e
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-15 14:29:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 API_ID = int(os.getenv("API_ID", "22834593"))
 API_HASH = os.getenv("API_HASH", "f400bc1d1baeb9ae93014ce3ee5ea835")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7386024380:AAEDZ5HvvQA1XrK_RILlHVrDoTvQNJXRYAw")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://AOMusicBot:AOMusicBot@cluster0.sibxiqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://AOMusic:AOMusic@cluster0.sibxiqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002134425165"))
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "6148346742").split()))
