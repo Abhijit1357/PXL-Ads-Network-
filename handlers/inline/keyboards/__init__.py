035dc382c4d819abdc73f1798bbf1f26852bd963
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-15 12:20:29 +0530

    Update __init__.py

--- handlers/inline/keyboards/__init__.py
+++ handlers/inline/keyboards/__init__.py
@@ -1,3 +1,4 @@
 from .start_keyboard import get_start_keyboard
 from .back_keyboard import get_back_keyboard
 from .publisher import get_publisher_panel_keyboard
+from .register import get_register_keyboard   # ← yeh line add karein
