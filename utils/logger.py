5fa91b0102cfacafc4c37085534e7db82890bae6
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-15 13:13:00 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,9 +1,8 @@
-from aiogram import Bot
-from config import LOG_GROUP_ID
+from config import LOG_CHANNEL
 
 async def log_to_group(bot: Bot, message: str):
-    if LOG_GROUP_ID:
+    if LOG_CHANNEL:
         try:
-            await bot.send_message(LOG_GROUP_ID, message, parse_mode="HTML")
+            await bot.send_message(LOG_CHANNEL, message, parse_mode="HTML")
         except Exception as e:
             print(f"[Logger Error] {e}")
