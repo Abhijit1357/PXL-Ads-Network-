ad4b0e944e8f560533d16b59b06598e06f842164
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-17 11:31:13 +0530

    Update register.py

--- handlers/inline/keyboards/register.py
+++ handlers/inline/keyboards/register.py
@@ -1,6 +1,10 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from db.models import is_registered_user
 
-def get_register_keyboard():
+async def get_register_keyboard(user_id: int):
+    #"""Only shows register button if user isn't registered"""
+    if await is_registered_user(user_id):
+        return None  # Returns None if user is registered
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="✅ Accept & Register", callback_data="register_accept")]
     ])
