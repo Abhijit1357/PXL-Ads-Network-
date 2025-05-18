022548983cecfd719d73d4d931f7b0ca1f1bcbc5
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-18 12:24:09 +0530

    Update register.py

--- handlers/inline/keyboards/register.py
+++ handlers/inline/keyboards/register.py
@@ -1,8 +1,9 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 async def get_register_keyboard(user_id: int):
-    keyboard = InlineKeyboardMarkup(row_width=1)
-    keyboard.add(
-        InlineKeyboardButton(text="📝 Register", callback_data=f"register_{user_id}")
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="Register", callback_data=f"register_{user_id}")]
+        ]
     )
     return keyboard
