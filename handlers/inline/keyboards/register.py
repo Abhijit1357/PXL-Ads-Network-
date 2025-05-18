1c703dd38c2ebf29295aca982941145821b828c3
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-18 12:53:37 +0530

    Update register.py

--- handlers/inline/keyboards/register.py
+++ handlers/inline/keyboards/register.py
@@ -4,5 +4,3 @@ def get_register_keyboard(user_id: int = None):
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="✅ Accept & Register", callback_data="register_accept")]
     ])
-
-reply_markup = get_register_keyboard(user_id=callback.from_user.id)
