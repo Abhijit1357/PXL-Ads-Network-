5284e81c2d6a732b1254219381d67f1e3a61516f
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-05-19 12:34:07 +0530

    Update __init__.py

--- handlers/inline/callbacks/__init__.py
+++ handlers/inline/callbacks/__init__.py
@@ -3,8 +3,9 @@ from .profile import router as profile_router
 from .panels import router as panels_router
 from .general import router as general_router
 from .publisher_panel import router as publisher_panel_router
-from .register import router as register_router   # <-- Register router import
-from .dashboard import router as dashboard_router  # Add this import
+from .register import router as register_router  # Register router import
+from .dashboard import router as dashboard_router  # Dashboard router import
+from .bot_monetization import router as bot_monetization_router  # Bot Monetization router import
 
 inline_callbacks_router = Router()
 
@@ -12,5 +13,6 @@ inline_callbacks_router.include_router(profile_router)
 inline_callbacks_router.include_router(panels_router)
 inline_callbacks_router.include_router(general_router)
 inline_callbacks_router.include_router(publisher_panel_router)
-inline_callbacks_router.include_router(register_router)  # <-- Register router included here
-inline_callbacks_router.include_router(dashboard_router)  # Register dashboard router
+inline_callbacks_router.include_router(register_router)  # Register router included
+inline_callbacks_router.include_router(dashboard_router)  # Dashboard router included
+inline_callbacks_router.include_router(bot_monetization_router)  # Bot Monetization router included
